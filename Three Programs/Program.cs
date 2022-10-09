d0b10006643933a80deb7deff4e375af79268677
igorvsx <igor.19840703@gmail.com>
2022-10-09 22:37:24 +0300

    Я опять сделал

--- Three Programs/Program.cs
+++ Three Programs/Program.cs
@@ -5,76 +5,87 @@
         static void Menu()
         {
             Console.WriteLine("Выберите программу, которую хотите запустить:" +
-                "\n 1. Угадай число" +
-                "\n 2. Таблица умножения" +
-                "\n 3. Вывод делителей числа" +
-                "\n 4. Выйти из программы");
+                " \n1. Угадай число" +
+                " \n2. Таблица умножения" +
+                " \n3. Вывод делителей числа" +
+                " \n4. Выйти из программы");
+        }
+        static void RandomNumber()
+        {
+            Random rand = new Random();
+            int value = rand.Next(0, 100);
+            Console.WriteLine("Угадай число от 0 до 100");
+            while (true)
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number < value)
+                {
+                    Console.WriteLine("Надо больше");
+                }
+                else if (number > value)
+                {
+                    Console.WriteLine("Надо меньше");
+                }
+                else
+                {
+                    Console.WriteLine("Вы угадали!!");
+                    break;
+                }
+            }
+        }
+        static void Table()
+        {
+            int[,] table = new int[10, 10];
+            for (int i = 1; i < table.GetLength(0); i++)
+            {
+                for (int j = 1; j < table.GetLength(1); j++)
+                {
+                    table[i, j] = i * j;
+                }
+            }
+            for (int i = 1; i < table.GetLength(0); i++)
+            {
+                for (int j = 1; j < table.GetLength(1); j++)
+                {
+                    Console.Write($"{table[i, j]} \t");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+        static void Divisors()
+        {
+            Console.WriteLine("Введите число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    Console.Write($"{i} \t");
+                }
+            }
+            Console.WriteLine();
         }
         static void Main()
         {
             Menu();
             int action = Convert.ToInt32(Console.ReadLine());
             while (action < 4)
-            { 
+            {
                 if (action == 1)
                 {
-                    Random rand = new Random();
-                    int value = rand.Next(0, 100);
-                    Console.WriteLine("Угадай число от 0 до 100");
-                    while (true)
-                    {
-                        int number = Convert.ToInt32(Console.ReadLine());
-                        if (number < value)
-                        {
-                            Console.WriteLine("Надо больше");
-                        }
-                        else if (number > value)
-                        {
-                            Console.WriteLine("Надо меньше");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Вы угадали!!");
-                            break;
-                        }
-                    }
-                    Main();
+                    RandomNumber();
                 }
                 else if (action == 2)
                 {
-                    int[,] table = new int[10, 10];
-                    for (int i = 1; i < table.GetLength(0); i++)
-                    {
-                        for (int j = 1; j < table.GetLength(1); j++)
-                        {
-                            table[i, j] = i * j;
-                        }
-                    }
-                    for (int i = 1; i < table.GetLength(0); i++)
-                    {
-                        for (int j = 1; j < table.GetLength(1); j++)
-                        {
-                            Console.Write($"{table[i, j]} \t");
-                        }
-                        Console.WriteLine();
-                        Console.WriteLine();
-                    }
-                    Main();
+                    Table();
                 }
                 else if (action == 3)
                 {
-                    Console.WriteLine("Введите число");
-                    int a = Convert.ToInt32(Console.ReadLine());
-                    for (int i = 1; i <= a; i++)
-                    {
-                        if (a % i == 0)
-                        {
-                            Console.Write($"{i} \t");
-                        }
-                    }
-                    Console.WriteLine();
-                    Main();
+                    Divisors();
                 }
+                Menu();
+                action = Convert.ToInt32(Console.ReadLine());
             }
             Environment.Exit(0);
         }
